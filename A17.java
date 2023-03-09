955a77e02fff18ae1a3e4f195feb048583a12f88
siddesh140 <114944843+siddesh140@users.noreply.github.com>
2023-03-09 16:33:24 +0530

    Add files via upload

---
+++ A17.java
@@ -0,0 +1,14 @@
+package myproject1;
+import java.util.*;
+public class A17 {
+
+	public static void main(String[] args) {
+		Scanner Sc = new Scanner(System.in);
+		int a = Sc.nextInt();
+		int b = Sc.nextInt();
+		int sum = a+b;
+		System.out.print(sum);
+
+	}
+
+}
---
+++ Cj11.java
@@ -0,0 +1,10 @@
+
+public class Cj11 {
+
+	public static void main(String[] args) {
+	double a = 3.14;
+	System.out.print(a);
+
+	}
+
+}
